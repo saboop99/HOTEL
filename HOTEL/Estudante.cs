de9f5898aedd935f9ca2759356da40a1ffc8fdfb
saboop99 <rafa4021@hotmail.com>
2021-10-07 21:44:20 -0300

    Adicionar arquivos de projeto.

---
+++ HOTEL/Estudante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HOTEL
+{
+    //RAFAEL DO NASCIMENTO RODRIGUES 210211
+    
+    
+    //Criação da classe estudante
+    class Estudante
+    {
+        //utilização do encapsulamento autoproperties para a variavel Nome
+        public string Nome { get; set; }
+
+        //utilização do encapsulamento autoproperties para a variavel Email
+        public string Email { get; set; }
+
+        //Criação do método construtor com os paramentros nome e email e variaveis locais Nome e Email
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        //método override para sobrescrever e mostrar os dados na tela atulizados com os que usuario digitou (Nome e Email dos estudantes no hotel)
+        public override string ToString()
+        {
+            return Nome + "," + Email;
+        }
+    }
+}
---
+++ HOTEL/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace HOTEL
+{
+    class Program
+    {
+        //RAFAEL DO NASCIMENTO RODRIGUES 210211
+
+
+        static void Main(string[] args)
+        {
+            //Declaração da matriz Estudante, com tamanho 10
+            Estudante[] vect = new Estudante[10];
+
+            //pedir ao usuário digitar a quantidade de quartos alugados, do total de 10
+            Console.WriteLine("Quantos quartos vão ser alugados?");
+            //conversão e leitura do dado digitado para int
+            int n = int.Parse(Console.ReadLine());
+
+            //condicional para a matriz (começa no 1[pois o quarto começa no 1 e não no 0], é menor ou igual ao numero de quartos digitados, vai acrescentando na vairavel i)
+            for (int i = 1; i <= n; i++)
+            {
+                //pular linha
+                Console.WriteLine();
+                
+                //mostrar o numero do hospede, como a variavel i da condicional for é 1, a contagem aqui começa a partir do 1, então é mostrado na tela "Hospede #1"
+                Console.WriteLine($"Hospede #{i}");
+                //informar ao usuario para ele digitar o nome
+                Console.WriteLine("Nome");
+                //leitura e armazenamento dos dados digitados para string nome
+                string nome = Console.ReadLine();
+                //informar ao usuario para ele digitar o email
+                Console.WriteLine("Email");
+                //leitura e armazenamento dos dados digitaos para string email
+                string email = Console.ReadLine();
+                //informar ao usuario para ele digitar o numero do quarto
+                Console.WriteLine("Quarto: ");
+                //leitura e conversão dos dados digitados para int
+                int quarto = int.Parse(Console.ReadLine());
+                //criação do vetor vect[quarto] para instanciar o nome e o email inputados anteriormente, para o quarto alugado
+                vect[quarto] = new Estudante(nome, email);
+            }
+
+            //pular linha
+            Console.WriteLine();
+            //mostrar para o usuario que a proxima informação se trata dos quartos ocupados
+            Console.WriteLine("Quartos ocupados: ");
+            //condicional para a matriz (começa no 0, é menor que o numero de quartos totais, vai acrescentando na vairavel i)
+            for (int i = 0; i < 10; i++)
+            {
+                //condicional para se o numero do quarto for diferente de nulo (null para se deixar automaticamente os quartos em ordem numerica)
+                if(vect[i] != null)
+                {
+                    //mostrar ao usuario todos os quartos ocupados e seus respectivos ocupantes armazenados na variavel i
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+        }
+    }
+}
